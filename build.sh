c4656cfa74a49ef777c87b283c67274e701e063a
Sandeep <sandeep@opstree.com>
2023-10-02 21:10:21 +0530

    Added conditional check of if policy already exists

--- build.sh
+++ build.sh
@@ -11,7 +11,17 @@ sleep  $SLEEP_DURATION
 TASK_STATUS=0
 
 envsubst < policy.template > policy 
+POLICY_NAME=ot-${AWS_RESOURCE}-policy
+POLICY_ARN="arn:aws:iam::639033570077:policy/${POLICY_NAME}"
 
-createPolicy ot-${AWS_RESOURCE}-policy policy
+POLICY_EXISTS=`policyExists ${POLICY_ARN}`
+
+if [ "$POLICY_EXISTS" -eq 0 ]
+then
+    logWarningMessage "Policy with ARN ${POLICY_ARN} already exists"
+else
+    logInfoMessage "Creating Policy with ARN ${POLICY_ARN}"
+    createPolicy ${POLICY_NAME} policy
+fi
 
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
